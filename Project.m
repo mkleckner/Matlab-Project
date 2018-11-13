e06a849fe04e251f390aa80db9ab38b2a08942f2
mkleckner <44987633+mkleckner@users.noreply.github.com>
2018-11-12 21:12:04 -0500

    Update Project.m

--- Project.m
+++ Project.m
@@ -1,13 +1,15 @@
 % Mitchell Kleckner and Daniel Nguyen
 
+% import violent crime and vacant building data by neighborhood
+[numbsv namesv datav] = xlsread('Vacant_Buildings_Data.xlsx');
+[numbsc namesc datac] = xlsread('Violent_Crime_2017.xlsx');
 
-[numbsv namesv datav] = xlsread('Vacant_Buildings.xlsx');
-[numbsc namesc datac] = xlsread('BPD_Part_1_Victim_Based_Crime_Data.xlsx');
+% create vectors with the neighborhoods from each array for future analysis
+neighv = datav(:,2);
+neighc = datac(:,2);
 
-neighv = datav(:,1);
-neighc = datac(:,1);
-
-uniqueneighv = unique(neighv);
-uniqueneighc = unique(neighc);
-
-X = repmat(neighv,length(uniqueneighv),1);
+% create an array with the name of each unique neighborhood, the number of
+% occurrences within the respective arrays, and the percentage out of total
+% occurrence
+tneighv = tabulate(neighv);
+tneighc = tabulate(neighc);
