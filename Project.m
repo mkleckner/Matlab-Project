6dd1f90c77664c596079247cb0b3fa3d9a9045ee
mkleckner <44987633+mkleckner@users.noreply.github.com>
2018-11-12 20:02:26 -0500

    Update Project.m

--- Project.m
+++ Project.m
@@ -4,8 +4,8 @@
 [numbsv namesv datav] = xlsread('Vacant_Buildings.xlsx');
 [numbsc namesc datac] = xlsread('BPD_Part_1_Victim_Based_Crime_Data.xlsx');
 
-neighv = datav(:,6);
-neighc = datac(:,10);
+neighv = datav(:,1);
+neighc = datac(:,1);
 
 uniqueneighv = unique(neighv);
 uniqueneighc = unique(neighc);
